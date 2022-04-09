d6c06fbfebc1b9e75c1078aca32dd1c3f8b83247
lancy <vshust505@gmail.com>
2022-04-09 10:04:53 +0300

    Initial commit

---
+++ app/src/main/java/by/bsuir/vshu/relaxapp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package by.bsuir.vshu.relaxapp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/by/bsuir/vshu/relaxapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package by.bsuir.vshu.relaxapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
