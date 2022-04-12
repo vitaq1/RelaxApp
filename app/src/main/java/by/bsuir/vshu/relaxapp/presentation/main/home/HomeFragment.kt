6ed81d823b8ab3fb25834bb48666f4f870c09560
lancy <vshust505@gmail.com>
2022-04-12 13:04:34 +0300

    add fonts, login activity

--- app/src/main/java/by/bsuir/vshu/relaxapp/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/by/bsuir/vshu/relaxapp/presentation/main/home/HomeFragment.kt
@@ -25,7 +25,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
     }
+
 }
--- app/src/main/java/by/bsuir/vshu/relaxapp/presentation/onboarding/OnboardingActivity.kt
+++ app/src/main/java/by/bsuir/vshu/relaxapp/presentation/onboarding/OnboardingActivity.kt
@@ -1,12 +1,39 @@
 package by.bsuir.vshu.relaxapp.presentation.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import by.bsuir.vshu.relaxapp.R
+import by.bsuir.vshu.relaxapp.presentation.login.LoginActivity
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var signInButton: Button
+    private lateinit var signUpButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        initViews()
     }
+
+
+    private fun initViews(){
+        signInButton = findViewById(R.id.signInButton)
+        signInButton.apply { setOnClickListener { openLoginActivity() } }
+        signUpButton = findViewById(R.id.signUpButton)
+        signUpButton.apply { setOnClickListener { openLoginActivity() } }
+    }
+
+    private fun openLoginActivity(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
 }
